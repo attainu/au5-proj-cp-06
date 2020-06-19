a52714777e0d9071726000f5ff7a716921476eda
Ameet Pradhan <jimmy.4244@gmail.com>
2020-06-19 22:49:34 +0530

    ENV_VARIABLES added

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const helmet = require("helmet");
-const cors = require('cors')
-
+const helmet = require('helmet');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -11,12 +11,24 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
+});
 
 module.exports = app;
