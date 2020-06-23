fe0eb6036b9cfbb5efaeaa54efdcaf89dc7fdd1b
Ameet Pradhan <jimmy.4244@gmail.com>
2020-06-24 04:17:48 +0530

    add ENV

--- firebase-fn/functions/index.js
+++ firebase-fn/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-
+require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
 
--- firebase-fn/functions/util/config.js
+++ firebase-fn/functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: 'AIzaSyD156Y4RhHeYnW0xodrUMrFUCKvWOSlYok',
+  apiKey: process.env.API_KEY,
   authDomain: 'fakebook-3d144.firebaseapp.com',
   databaseURL: 'https://fakebook-3d144.firebaseio.com',
   projectId: 'fakebook-3d144',
   storageBucket: 'fakebook-3d144.appspot.com',
   messagingSenderId: '360752043762',
-  appId: '1:360752043762:web:ab2515d093969b86c1626b',
+  appId: process.env.APP_ID,
   measurementId: 'G-1WFZTEK5FR'
 };
