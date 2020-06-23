a0ff2ec93ce5b85e9b210048e460aa2e4e71a92d
Ameet Pradhan <jimmy.4244@gmail.com>
2020-06-24 04:03:16 +0530

    final commit

---
+++ firebase-fn/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyD156Y4RhHeYnW0xodrUMrFUCKvWOSlYok',
+  authDomain: 'fakebook-3d144.firebaseapp.com',
+  databaseURL: 'https://fakebook-3d144.firebaseio.com',
+  projectId: 'fakebook-3d144',
+  storageBucket: 'fakebook-3d144.appspot.com',
+  messagingSenderId: '360752043762',
+  appId: '1:360752043762:web:ab2515d093969b86c1626b',
+  measurementId: 'G-1WFZTEK5FR'
+};
